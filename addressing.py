25b27af91042bbde9b717c7c1f1d7d5af10c026e
Gloria <JYWang0411@gmail.com>
2017-10-30 22:26:31 +0800

    add the Addressing Mode check function
    a question still left: for the 5th mode, can the displacement be None?

--- addressing.py
+++ addressing.py
@@ -1,11 +1,38 @@
 # -*- coding:UTF-8 -*-
-import sys
+import sys, re
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 class Addressing():
 	@staticmethod
 	def checkMode(op):
+		""" 
+			ModeCode	Description
+			   1		Immediate
+			   2		Register
+			   3		Displacement (absolute/static address)
+			   4		Base
+			   5		Base + Displacement -- Can this Displacement be None?
+			   6		(Index * Scale) + Displacement
+			   7		Base + Index + Displacement
+			   8		Base + (Index * Scale) + Displacement
+		"""
+		if re.match(r'\$0x\w+$', op, re.I):
+			return 1
+		elif re.match(r'%\w+$', op, re.I):
+			return 2
+		elif re.match(r'0x\w+$', op, re.I):
+			return 3
+		elif re.match(r'\(%\w+\)$', op, re.I):
+			return 4
+		elif re.match(r'-?0x\w+\(%\w+\)$', op, re.I):
+			return 5
+		elif re.match(r'(-?0x\w+)?\(,%\w+,\w+\)$', op, re.I):
+			return 6
+		elif re.match(r'(-?0x\w+)?\(%\w+,%\w+\)$', op, re.I):
+			return 7
+		elif re.match(r'(-?0x\w+)?\(%\w+,%\w+,\w+\)$', op, re.I):
+			return 8
 		return
 	
 	@staticmethod
